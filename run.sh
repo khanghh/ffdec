b068a6fc051ce33cadf1a85c68c2657cd09a834f
Hoang Hong Khang <hoanghongkhang@gmail.com>
2018-01-30 23:15:45 +0700

    update 20180131

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-java -Xmx2560M -jar ffdec.jar -cli -timeout 999999 -exportTimeout 999999 -exportFileTimeout 999999 -export script out/swf_20180121 swf/swf_20180120
+version='20180131'
+java -Xmx2560M -jar ffdec.jar -cli -timeout 999999 -exportTimeout 999999 -exportFileTimeout 999999 -export script out/swf_$version swf/swf_$version
