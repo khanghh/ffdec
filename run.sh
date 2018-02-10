dfd9e5eaead92bb563dd37811fd7ec205391cef2
Khang Hoang <hoanghongkhang@gmail.com>
2018-02-10 16:53:33 +0700

    version 20180210

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 version='20180210'
+./EnDecryptPNG swf/swf_$version/2.png
+./EnDecryptPNG swf/swf_$version/4.png
 java -Xmx2560M -jar ffdec.jar -cli -timeout 999999 -exportTimeout 999999 -exportFileTimeout 999999 -export script out/swf_$version swf/swf_$version
