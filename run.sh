1c13a56d1fd8444de7fb62bf0b1562436e6b9058
Khang Hoang <hoanghongkhang@gmail.com>
2018-02-10 15:47:55 +0700

    add swf_20180210

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-version='20180131'
+version='20180210'
 java -Xmx2560M -jar ffdec.jar -cli -timeout 999999 -exportTimeout 999999 -exportFileTimeout 999999 -export script out/swf_$version swf/swf_$version
